48d39a62cac67cbaba260bd4fb387b56b5981d37
Anand Swaminathan <aswamina@gmail.com>
2018-11-20 19:58:46 -0600

    changes to invoke a selectedlistitem component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import EnhancedTable from './components/PaginatedSortableTable/EnhancedTable';
+import SelectedListItem from "./components/LegendFilter/SelectedListItem";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <EnhancedTable/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div style={styles} className="App">
+                <SelectedListItem/>
+                <EnhancedTable/>
+            </div>
+        );
+    }
 }
 
 export default App;
 
 
+const styles = {
+    display:'flex',
+    direction:'row',
+    //background: 'linear-gradient(45deg, #bebebe 30%, #bebebe 90%)',
+};
+
+
 /*
 import Select from './Select';
+import ThemedRadioButtonsGroup from "./components/RadioGroup/RadioButtonsGroup";
+<ThemedRadioButtonsGroup/>
 <Select/>
+background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
  */
